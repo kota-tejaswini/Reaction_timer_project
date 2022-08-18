0c383d9e46134e9bd8335d793dc36cad7e3045b1
kota-tejaswini <81311100+kota-tejaswini@users.noreply.github.com>
2022-08-18 16:00:30 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+ <h1>
+ REACTION TIMER
+ </h1>
+ <button @click="genrate">play</button>
+ <br>
+ <h3>
+ About this game:
+ after uh pressed the button after some random time u can see a box down uh have to waitfor that box to come and uh have to press that as fast as uh can.u are ranked based on the time uh pressed after the box pops
+ </h3>
+ <br>
+ <br>
+ <br>
+ <h3 v-if="xu">
+ <h3 v-if="!isplaying">you pressed in {{count}} milliseconds </h3></h3>
+
+ <br>
+ <br>
+ <br>
+ <br>
+ <center><HelloWorld v-if="isplaying" :time="time" @teju="xuy"></HelloWorld></center>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      isplaying:false,
+      time:null,
+      xu:false,
+      count:0
+    }
+  },
+  methods: {
+     genrate(){
+       console.log("hii");
+       this.time=2000+(Math.random()*5000);
+       this.isplaying=true;
+     },
+      xuy(tuf){
+this.count=tuf;
+this.xu=true;
+this.isplaying=false;
+  }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  color:green;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+<div @click="gen" v-if="boo" >
+<center >
+Click as fast as you can</center>
+</div>
+</template>
+
+<script>
+export default {
+ 
+  props: ["time"],
+  data(){
+    return{
+      se:null,
+      arc:0,
+      boo:false
+    }
+  },
+  mounted(){
+    console.log(this.time);
+    var v=this;
+           setTimeout(
+           function(){ v.start();}
+           ,this.time);
+  },
+  updated(){
+     console.log(this.time);
+    var v=this;
+           setTimeout(
+           function(){ v.start();}
+           ,this.time);
+  },
+  methods:{
+     gen(){
+           clearInterval(this.se);
+           console.log(this.arc);
+            this.boo=false;
+            this.$emit('teju',this.arc);
+          
+          
+          
+           
+
+     },
+     start(){
+       this.boo=true
+        this.arc=0;
+          this.se=setInterval(()=>{this.arc=this.arc+10},10)
+     }
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+div{
+  border:white;
+  background-color:green;
+  border-radius:8;
+  height:500px;
+  width:500px;
+}
+</style>
